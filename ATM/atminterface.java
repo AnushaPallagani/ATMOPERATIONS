6b29b4f2e2ac65a1f18b57b16fae41f5bb67b509
AnushaPallagani <127949328+AnushaPallagani@users.noreply.github.com>
2023-03-21 16:02:45 +0530

    Update atminterface.java

--- ATM/atminterface.java
+++ ATM/atminterface.java
@@ -17,17 +17,19 @@ public static void main(String[] args) {
 		
 		//import required classes and packages   
 		
-		//create ATMExample class to implement the ATM functionality  
-//		public class atminterface
-//		{  
-//		    //main method starts   
-//		    public static void main(String args[] )  
-//		    {  
+		
+
+
+
+//		
+
+//		  
+//		      
 //		        //declare and initialize balance, withdraw, and deposit  
-//		        int balance = 100000, withdraw, deposit;  
+//		        
 //		          
 //		        //create scanner class object to get choice of user  
-//		        Scanner sc = new Scanner(System.in);
+//		       
 		int balance=5000,withdraw,deposite;
 		Scanner sc=new Scanner(System.in);
 //		          
